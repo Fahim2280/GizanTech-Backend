9cdc39b9b42d3ddc2393aa2d996c7d035795d86e
Md. Fahim Alam <kfahim2280@gmail.com>
2024-12-27 16:01:47 +0600

    update

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+  const cors = require('cors');
 
   app.useLogger(logger);
-  app.enableCors();
+  //app.enableCors();
+  app.use(cors());
   app.listen(7000).then(() => {
     logger.log('Application is running on: http://localhost:7000');
   });
--- src/temperature/temperature.gateway.ts
+++ src/temperature/temperature.gateway.ts
@@ -1,34 +1,50 @@
 import {
   WebSocketGateway,
   WebSocketServer,
+  OnGatewayInit,
   OnGatewayConnection,
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { Injectable } from '@nestjs/common';
-import { TemperatureService } from './temperature.service';
 
-@WebSocketGateway()
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST'],
+  },
+})
 export class TemperatureGateway
-  implements OnGatewayConnection, OnGatewayDisconnect
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  @WebSocketServer() server: Server;
+  @WebSocketServer()
+  server: Server;
 
-  constructor(private readonly temperatureService: TemperatureService) {}
+  private temperatureData = [
+    { timestamp: new Date().toISOString(), temperature: 25 },
+  ];
+
+  emitTemperatureUpdates() {
+    setInterval(() => {
+      const newData = {
+        timestamp: new Date().toISOString(),
+        temperature: Math.floor(Math.random() * 40), 
+      };
+      this.temperatureData.push(newData);
+
+      this.server.emit('temperatureUpdate', newData);
+    }, 5000);
+  }
+
+  afterInit() {
+    console.log('Temperature WebSocket Gateway initialized');
+    this.emitTemperatureUpdates();
+  }
 
   handleConnection(client: any) {
-    console.log(`Client connected: ${client.id}`);
+    console.log('Client connected:', client.id);
   }
 
   handleDisconnect(client: any) {
-    console.log(`Client disconnected: ${client.id}`);
-  }
-
-  broadcastTemperature() {
-    const temperature = this.temperatureService.generateRandomTemperature();
-    this.server.emit('temperatureUpdate', {
-      temperature,
-      timestamp: new Date().toISOString(),
-    });
+    console.log('Client disconnected:', client.id);
   }
 }
